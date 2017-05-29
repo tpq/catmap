94ff0a4944a7196ae5bb6be05325839bf2e14436
Thom Quinn <contacttomquinn@gmail.com>
2017-05-29 22:48:44 +1000

    Rebuild catmap.funnel using metafor package

--- R/funnel.R
+++ R/funnel.R
@@ -1,44 +1,35 @@
-#' Funnel Plots for catmap
+#' catmap: Funnel Plot
 #'
-#' \code{catmap.funnel} creates a funnel plot of the individual ORs against the
-#' se and saves the file to the current working directory (use getwd() to
-#' view).  The plots are not created in the R Graphics device.
+#' The \code{catmap.funnel} creates a funnel plot of the individual Log Odds Ratio
+#'  against the standard error of the Log Odds Ratio. The vertical line indicates
+#'  the combined Log Odds Ratio. Per the \code{metafor} package, "A pseudo
+#'  confidence interval region is drawn around this value with bounds equal
+#'  to ± 1.96 SE".
 #'
-#' \code{catmap.funnel} creates a .pdf file of the funnel plot. Plots are not
-#' created in the R Graphics device window, but are instead saved to a .pdf
-#' file in the current working directory.
+#' @param catmapobject A catmap object created by \code{\link{catmap}}.
+#' @param funnel A boolean. Toggles whether the funnel plot should get saved
+#'  to the current working directory.
 #'
-#' @param catmapobject The catmap object created by a previous call to catmap
-#' @param funnel Logical.  Should a funnel plot be produced?  Funnel plots plot
-#' standard error of the log ORs against the ORs along with a solid line at 1.0
-#' and a dotted line at the overall pooled OR.  Used to assess publication
-#' bias.  Output plot file is saved with the default name of
-#' \bold{dataset.funnel.plot.pdf} where dataset is the name of the file given
-#' as the first argument to catmap.
-#' @author Kristin K. Nicodemus, \email{kristin.nicodemus@@well.ox.ac.uk}
-#' @seealso \code{\link{catmap}}, \code{\link{catmap.sense}},
-#' \code{\link{catmap.cumulative}}, \code{\link{catmap.forest}}.
-#' @keywords methods
+#' @author Algorithm designed and implemented by Kristin K. Nicodemus.
+#'  Code modified and updated by Thom Quinn.
+#' @seealso \code{\link{catmap}}, \code{\link{catmap.forest}},
+#'  \code{\link{catmap.sense}}, \code{\link{catmap.cumulative}},
+#'  \code{\link{catmap.funnel}}
+#'
 #' @examples
 #' data(catmapdata)
 #' catmapobject <- catmap(catmapdata, 0.95, TRUE)
 #' catmap.funnel(catmapobject, TRUE)
 #' @export
-catmap.funnel<-function(catmapobject, funnel){
-  if(funnel==TRUE){
-    if(catmapobject$dataset!=catmapdata){
-      pdf(file=(paste(catmapobject$dataset, "funnel.plot.pdf", sep=".")))
-    }
-    if(catmapobject$dataset==catmapdata){
-      pdf(file=(paste("catmapdata", "funnel.plot.pdf", sep=".")))
-    }
-    OddsRatio<-exp(catmapobject$logOR)
-    fun.se<-sqrt(catmapobject$comvarlogOR)
-    maxse<-max(fun.se)
-    minse<-min(fun.se)
-    plot(OddsRatio, fun.se, ylim=rev(c(minse,maxse)),log="x", ylab="Standard Error Log OR")
-    abline(v=1)
-    abline(v=catmapobject$combinedOR, lty=2)
-    graphics.off()
-  }
+catmap.funnel<-function(catmapobject, funnel = FALSE){
+
+  # The original catmap.funnel function plotted SE of Log OR
+  #  against exp(Log OR), using $combinedOR as reference line
+  # Here, we plot SE of Log OR against Log OR itself, using
+  #  $combinedLogOR as reference line
+  if(funnel) pdf(file = paste0(catmapobject$dataset, ".funnel.plot.pdf"))
+  m <- metafor::funnel(x = catmapobject$logOR, sei = sqrt(catmapobject$comvarlogOR),
+                       refline = catmapobject$combinedLogOR, xlab = "Log Odds Ratio")
+  if(funnel) graphics.off()
+  return(m)
 }
--- tests/testthat/test_funnel.R
+++ tests/testthat/test_funnel.R
@@ -1,5 +1,6 @@
 test_that("catmap.funnel works", {
-  expect_equal(data(catmapdata), )
+  expect_equal(data(catmapdata), "catmapdata")
   catmapobject <- catmap(catmapdata, 0.95, TRUE)
-  expect_equal(catmap.funnel(catmapobject, TRUE), )
+  expect_equal(catmap.funnel(catmapobject, TRUE)$x, catmapobject$logOR)
+  expect_equal(catmap.funnel(catmapobject, TRUE)$y, sqrt(catmapobject$comvarlogOR))
 })
