847614f6ec38cb2d086b7a3f069e111fb7929363
Thom Quinn <contacttomquinn@gmail.com>
2017-06-11 18:35:59 +1000

    Build all figures using catmap 1.6.4

---
+++ data-raw/catmap164/run.R
@@ -0,0 +1,14 @@
+# Name the working directory where your file is located:
+setwd("./data-raw/catmap164")
+
+library(catmap)
+data(catmapdata)
+
+# Build cm.obj from file location
+cm.obj <- catmap(catmapdata, 0.95, TRUE)
+
+# Call catmap functions
+catmap.forest(cm.obj, TRUE, TRUE)
+catmap.sense(cm.obj, TRUE, TRUE, TRUE)
+catmap.cumulative(cm.obj, TRUE, TRUE, TRUE)
+catmap.funnel(cm.obj, TRUE)
